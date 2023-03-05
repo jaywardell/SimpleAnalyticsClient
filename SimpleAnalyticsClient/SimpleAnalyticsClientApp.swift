0cfac7bb9eca9aff29c0ebdec20d618dbce7a252
Joseph Wardell <jaywardell@mac.com>
2023-03-05 08:53:41 -0500

    Initial Commit

---
+++ SimpleAnalyticsClient/SimpleAnalyticsClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAnalyticsClientApp.swift
+//  SimpleAnalyticsClient
+//
+//  Created by Joseph Wardell on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAnalyticsClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
