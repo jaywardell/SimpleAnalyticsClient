8423fd9781f41b2d9b8d7446f5ee24a471060be0
Joseph Wardell <jaywardell@mac.com>
2023-03-20 20:55:59 -0400

    refactored the flag selection code into a separate view

---
+++ SimpleAnalyticsClient/FilterByBoolView.swift
@@ -0,0 +1,35 @@
+//
+//  FilterByBoolView.swift
+//  SimpleAnalyticsClient
+//
+//  Created by Joseph Wardell on 3/20/23.
+//
+
+import SwiftUI
+
+struct FilterByBoolView: View {
+    @Binding var value: Bool?
+        
+    var body: some View {
+        let matchFlag = Binding<Bool>(
+            get: { value != nil },
+            set: { value = ($0 == true) ? false : nil }
+        )
+        
+        let flagValue = Binding<Bool>(
+            get: { value ?? false },
+            set: { value = $0 }
+        )
+
+        return HStack {
+            
+            Toggle(isOn: matchFlag) {
+                Text("")
+            }
+            Toggle(isOn: flagValue) {
+                Text("Flag")
+            }
+            .disabled(!matchFlag.wrappedValue)
+        }
+    }
+}
--- SimpleAnalyticsClient/FilterView.swift
+++ SimpleAnalyticsClient/FilterView.swift
@@ -10,35 +10,40 @@ import SwiftUI
 struct FilterView: View {
     
     @State private var flag: Bool?
-    
-
-    
+  
     var body: some View {
-        let matchFlag = Binding<Bool>(
-            get: { flag != nil },
-            set: { flag = ($0 == true) ? false : nil }
-        )
-        
-        let flagValue = Binding<Bool>(
-            get: { flag ?? false },
-            set: { flag = $0 }
-        )
-
-        return VStack(alignment: .leading) {
-            
-            Toggle(isOn: matchFlag) {
-                Text("Filter by Flag")
+        FilterByBoolView(value: $flag)
+            .onChange(of: flag) { newValue in
+                print(newValue)
             }
-            Toggle(isOn: flagValue) {
-                Text("Flag")
-            }
-            .padding(.leading)
-            .disabled(!matchFlag.wrappedValue)
-        }
-        .onChange(of: flag) { newValue in
-            print(newValue)
-        }
     }
+    
+//    var body: some View {
+//        let matchFlag = Binding<Bool>(
+//            get: { flag != nil },
+//            set: { flag = ($0 == true) ? false : nil }
+//        )
+//
+//        let flagValue = Binding<Bool>(
+//            get: { flag ?? false },
+//            set: { flag = $0 }
+//        )
+//
+//        return HStack {
+//
+//            Toggle(isOn: matchFlag) {
+//                Text("")
+//            }
+//            Toggle(isOn: flagValue) {
+//                Text("Flag")
+//            }
+////            .padding(.leading)
+//            .disabled(!matchFlag.wrappedValue)
+//        }
+//        .onChange(of: flag) { newValue in
+//            print(newValue)
+//        }
+//    }
 }
 
 struct FilterView_Previews: PreviewProvider {
